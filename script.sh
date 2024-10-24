9be5cb6b5262c21acdedadc0a892523461b2a4a6
Ryuunosuke Example <exryuske@proton.me>
2024-10-25 01:02:35 +0500

    2

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 if [ "$1" = "bender"  ] || [ "$1" = "anton"  ]; then
     if [ "$2" = "install" ]; then
         nix-shell -p git --command "git clone https://github.com/ExRyuske/dotfiles.git && exit"
-        sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ~/dotfiles/hosts/"$1"/disko.nix
+        sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ~/dotfiles/hosts/$1/disko.nix
         sudo nixos-generate-config --no-filesystems --root /mnt
         sudo rm /mnt/etc/nixos/configuration.nix
         sudo mv ~/dotfiles/* /mnt/etc/nixos/
