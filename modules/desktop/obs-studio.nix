46ded9083cf3399c49f44b9d8fa4d4727001772c
Ryuunosuke Example <exryuske@proton.me>
2024-10-27 02:28:40 +0500

    15

--- modules/desktop/obs-studio.nix
+++ modules/desktop/obs-studio.nix
@@ -1,6 +1,6 @@
 { inputs, config, pkgs, lib, ... }: { # https://wiki.nixos.org/wiki/OBS_Studio
   programs.obs-studio = {
     enable = true;
-    plugins = with pkgs.obs-studio-plugins; [ obs-pipewire-audio-capture ]
+    plugins = with pkgs.obs-studio-plugins; [ obs-pipewire-audio-capture ];
   };
 }
