73359f6b16093a7bb4129a409735a1fb23b6e30c
Ryuunosuke Example <exryuske@proton.me>
2024-10-27 02:30:32 +0500

    16

--- modules/desktop/virt-manager.nix
+++ modules/desktop/virt-manager.nix
@@ -1,12 +1,16 @@
-{ inputs, config, pkgs, lib, ... }: { # https://wiki.nixos.org/wiki/Virt-manager https://wiki.nixos.org/wiki/Libvirt
+{ inputs, config, pkgs, lib, ... }: { # https://wiki.nixos.org/wiki/Libvirt
   virtualisation = {
     libvirtd = {
       enable = true;
-      swtpm.enable = true;
-      ovmf.packages = with pkgs; [ OVMFFull.fd ];
+      qemu = {
+        swtpm.enable = true;
+        ovmf.packages = with pkgs; [ OVMFFull.fd ];
+      };
     };
+
     spiceUSBRedirection.enable = true;
   };
 
+  # https://wiki.nixos.org/wiki/Virt-manager
   programs.virt-manager.enable = true;
 }
