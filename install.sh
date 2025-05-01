15bec405673b0e894b1ad275f6f263bdf84d0bb6
Ryuunosuke <120612156+ExRyuske@users.noreply.github.com>
2025-05-01 22:26:28 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 nix-shell -p git --command "git clone https://github.com/ExRyuske/dotfiles.git && exit"
 sudo mv ~/dotfiles/* /etc/nixos/
-rm ~/dotfiles
+rm -r ~/dotfiles
 sudo nixos-rebuild switch
+sudo reboot
