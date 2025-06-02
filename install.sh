51b367c42b2e3bc9f06eeec970ce4fff92555806
Ryuunosuke <120612156+ExRyuske@users.noreply.github.com>
2025-06-03 03:33:02 +0500

    23

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 nix-shell -p git --command "git clone https://github.com/ExRyuske/dotfiles.git && exit"
-sudo nixos-generate-config --no-filesystems --root ./
-sudo mv ./etc/nixos/hardware-configuration.nix ./dotfiles
+#sudo nixos-generate-config --no-filesystems --root ./
+#sudo mv ./etc/nixos/hardware-configuration.nix ./dotfiles
 #sudo nix --experimental-features 'nix-command flakes' run 'github:nix-community/disko/latest#disko-install' -- --write-efi-boot-entries --flake ./dotfiles#desktop --disk main /dev/sda
+
 sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode disko ./dotfiles/disko.nix --disk main /dev/sda
-sudo nixos-install --no-channel-copy --flake ./dotfiles#desktop
+sudo nixos-generate-config --no-filesystems --root /mnt
+sudo mv ./dotfiles* /mnt/etc/nixos
+sudo nixos-install --no-channel-copy --flake /mnt/etc/nixos#desktop
 #reboot
