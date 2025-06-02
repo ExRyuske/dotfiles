f3de02c465e1c7aea2a752161b9f0a8e956a3a72
Ryuunosuke <120612156+ExRyuske@users.noreply.github.com>
2025-06-03 01:54:49 +0500

    16

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 nix-shell -p git --command "git clone https://github.com/ExRyuske/dotfiles.git && exit"
 sudo nixos-generate-config --no-filesystems --root /mnt
-sudo vm ./dotfiles/* /mnt/etc/nixos
+sudo mv ./dotfiles/* /mnt/etc/nixos
 sudo nixos-install --flake /mnt/etc/nixos#desktop
 #reboot
